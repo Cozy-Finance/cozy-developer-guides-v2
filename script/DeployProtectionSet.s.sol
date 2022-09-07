f70c4d8bc117cc6062cf967b2996601d3f057c57
chad-js <chadflemies@gmail.com>
2022-09-07 15:51:07 -0400

    chore: deploy set

--- script/DeployProtectionSet.s.sol
+++ script/DeployProtectionSet.s.sol
@@ -41,24 +41,45 @@ contract DeployProtectionSet is Script, ScriptUtils {
   // -------- Cozy Contracts --------
 
   // Address of the Cozy protocol Manager.
-  IManager manager = IManager(address(0xBEEF));
+  IManager manager = IManager(address(0xc073F373F207a77759fb2184b1CFE1DDd4598D65));
 
   // -------- Market Info --------
 
   // The trigger addresses for each market in the set.
-  address[] triggers = [address(0xBEEF), address(0xBEEF)];
+  address[] triggers = [
+    address(0xF9FeD7853dC6F3E74421606F79a360DAE9256Cc3),
+    address(0xb7Cd2e5A6d02983f258A06E26baAF310054B14B1),
+    address(0x2f150A966d32ed8928200136d1D30A15c3694909),
+    address(0x5e3A0e3acBC61e54793256100cD9d9d87E86ce44),
+    address(0x6Fae18f377E58F62e089868eDF831f1d22114f14),
+    address(0x8b566Ee7e34c80E9dB2c1D6AB17652388d91aB05)
+  ];
 
   // The cost models for each market in the set. The indices of this array map 1:1 with the triggers array.
-  ICostModel[] costModels = [ICostModel(address(0xBEEF)), ICostModel(address(0xBEEF))];
+  ICostModel[] costModels = [
+    ICostModel(address(0x7e5a2bDC10F05D6cF15563570Eae3B8d346B9991)),
+    ICostModel(address(0x7e5a2bDC10F05D6cF15563570Eae3B8d346B9991)),
+    ICostModel(address(0x7e5a2bDC10F05D6cF15563570Eae3B8d346B9991)),
+    ICostModel(address(0x7e5a2bDC10F05D6cF15563570Eae3B8d346B9991)),
+    ICostModel(address(0x7e5a2bDC10F05D6cF15563570Eae3B8d346B9991)),
+    ICostModel(address(0x7e5a2bDC10F05D6cF15563570Eae3B8d346B9991))
+  ];
 
   // The weights for each market. The indices of this array map 1:1 with the triggers array.
   // NOTE: Weights are denoted in zoc (1e4). For example, 4000 is equivalent to 40%.
   // NOTE: The sum of weights must equal to 1 zoc (1e4).
-  uint16[] weights = [4000, 6000];
+  uint16[] weights = [1666, 1666, 1667, 1667, 1667, 1667];
 
   // The purchase fees for each market. The indices of this array map 1:1 with the triggers array.
   // NOTE: Purchase fees are denoted in zoc (1e4). For example, 50 is equivalent to 0.5%.
-  uint16[] purchaseFees = [50, 50];
+  uint16[] purchaseFees = [
+    250,
+    250,
+    250,
+    250,
+    250,
+    250
+  ];
 
   // -------- Set Configuration --------
 
@@ -70,19 +91,19 @@ contract DeployProtectionSet is Script, ScriptUtils {
   //      markets. Using the max leverage factor requires that all markets in the set have equal weights.
   //   3. The maximum leverage factor for a set is bounded by the max weight of all sets in a market, and is equal to
   //      `1 / max(weights)`. This means we need `leverageFactor / zoc > zoc / max(weights)`.
-  uint256 constant leverageFactor = 10000;
+  uint256 constant leverageFactor = 30000;
 
   // The fee charged by the Set owner on deposits.
-  uint256 constant depositFee = 100;
+  uint256 constant depositFee = 0;
 
   // Address of the set's decay model. The decay model governs how fast outstanding protection loses it's value.
-  IDecayModel constant decayModel = IDecayModel(address(0xBEEF));
+  IDecayModel constant decayModel = IDecayModel(address(0x09f20eA12fe5a1211A0485aa59C067E9fcC4c04A));
 
   // Address of the set's drip model. The drip model governs the interest rate earned by depositors.
-  IDripModel constant dripModel = IDripModel(address(0xBEEF));
+  IDripModel constant dripModel = IDripModel(address(0xEf778611eAf2e624432F49bcF7AC433584f642a2));
 
   // Address of the underlying asset of the set.
-  address constant asset = address(0xBEEF);
+  address constant asset = address(0x4200000000000000000000000000000000000006);
 
   // Arbitrary salt used for Set contract deploy.
   bytes32 constant salt = bytes32(hex"01");
@@ -90,10 +111,10 @@ contract DeployProtectionSet is Script, ScriptUtils {
   // -------- Set Authorized Roles --------
 
   // The owner of the set.
-  address owner = address(0xBEEF);
+  address owner = address(0x682bd405073dD248527E40184898eD45BB827527);
 
   // The pauser of the set.
-  address pauser = address(0xBEEF);
+  address pauser = address(0x682bd405073dD248527E40184898eD45BB827527);
 
   // ---------------------------
   // -------- Execution --------
